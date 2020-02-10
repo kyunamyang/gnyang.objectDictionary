f67c2caeb94f22f1b69b1ecec088d11abac666f4
KyuNam Yang <knyang@outlook.com>
2020-02-10 20:10:42 +0900

    Adding UI (ing)

--- Company.ObjectDictionary.Web/wwwroot/js/site.js
+++ Company.ObjectDictionary.Web/wwwroot/js/site.js
@@ -2,3 +2,52 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+var App = {};
+App.module = {};
+
+App.module.objectDictionary = function () {
+    var url = '';
+    var selectedAlphabet;
+    var models;
+
+    function setUrl(url) {
+        this.url = url;
+    }
+
+    function getCurrentAlphabet() {
+        return $('.selectedAlphabet').innerText;
+    }
+
+    function getModels() {
+        return this.models;
+    }
+
+    function setModels(m) {
+        this.models = m;
+    }
+
+    function getAll(callback) {
+        $.ajax({
+            context: this,
+            type: 'GET',
+            url: '/api/model',
+            data: '',
+            success: function (data) {
+                this.setModels(data);
+                callback();
+            },
+            error: function (req, status, error) {
+                alert(error);
+            },
+        });
+        
+    }
+
+    return {
+        setUrl: setUrl,
+        getModels: getModels,
+        setModels: setModels,
+        getAll: getAll,
+    }
+};
